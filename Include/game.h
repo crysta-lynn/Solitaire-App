5e7c518ecbc45a5e265b79cd4db66842d6dd899c
crysta-lynn <132976036+crysta-lynn@users.noreply.github.com>
2025-11-27 00:49:22 +0000

    made pile variables for game class

--- Include/game.h
+++ Include/game.h
@@ -1,12 +1,24 @@
 #pragma once
 
 #include "deck.h"
+#include <vector>
+#include <array>
 
-
+struct pile{ 
+    std:: vector<Card> cards;
+};
 
 class Game {
 private:
     Deck deck;
+
+    pile stock;
+    pile waste;
+    std::array<pile, 7> tableau;
+    std::array<pile, 4> foundation;
+
+public:
+    Game();
 
 };
 
